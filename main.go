303cb6f9e9a538002a0f4c002caa8b4e754151f5
Thrashy190 <72418715+Thrashy190@users.noreply.github.com>
2023-04-22 17:32:57 -0600

    Add go.mod, README.md and main.go

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+
+func roles(s*discordgo.Session, m*discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	//genera una carta con un header y un footer de prueba
+	if m.Content == "carta" {
+		embed := &discordgo.MessageEmbed{
+			Title: "Carta de prueba",
+			Description: "Esto es una carta de prueba",
+			Color: 0x00ff00,
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: "Footer de prueba",
+			},
+		}
+		s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	}
+}
+
+func hello(s*discordgo.Session, m*discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "Hello"{
+		s.ChannelMessageSend(m.ChannelID, "Hola, "+m.Author.Mention()+"!, Como estas?")
+	}
+}
+
+func ping(s*discordgo.Session, m*discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+}
+
+
+func main() {
+	discord, err := discordgo.New("Bot " + "MTA5OTQxOTc4MTI4ODk3MjM5MQ.GhrI1i.8_zXjYGp78bNXcWekQw1-hJSukYQVyadAZXtGI")
+	if err != nil {
+		panic(err)
+	}
+
+	discord.AddHandler(ping)
+	discord.AddHandler(hello)
+	discord.AddHandler(roles)
+
+	discord.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer discord.Close()
+
+	fmt.Println("Bot is running!")
+
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
